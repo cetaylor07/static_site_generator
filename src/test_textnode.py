65d2e2d685e51157a847717073e736912df893dc
cetaylor07 <cetaylor07@gmail.com>
2025-08-05 23:35:49 -0400

    Add ininitial TextNode Tests

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+    def test_noteq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.ITALIC_TEXT)
+        self.assertNotEqual(node, node2)
+    def test_noteq_different_url(self):
+        node = TextNode("Test Link", TextType.LINK, "http://boot.dev")
+        node2 = TextNode("Test Link", TextType.LINK, None)
+        self.assertNotEqual(node,node2)
+    def test_noteq_different_text(self):
+        node = TextNode("Test Link", TextType.LINK, "http://boot.dev")
+        node2 = TextNode("Different Text", TextType.LINK, "http://boot.dev")
+        self.assertNotEqual(node, node2)
+    def test_eq_all_equal(self):
+        node = TextNode("Testing", TextType.LINK, url=None )
+        node2 = TextNode("Testing", TextType.LINK, url=None )
+        self.assertEqual(node,node2)
+    
+if __name__ == "__main__":
+    unittest.main()

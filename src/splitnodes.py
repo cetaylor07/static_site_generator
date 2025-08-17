a761db0a7bb2c6d6fd411ce97586a56ba8b21074
cetaylor07 <cetaylor07@gmail.com>
2025-08-17 17:07:03 -0400

    Added mardown_to_blocks func

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -92,3 +92,13 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
     return matches
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
